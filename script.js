e0986ad002f3f1112499d5e11b7f8c41a8372d4a
Sung <sung272012@hotmail.com>
2024-07-26 22:33:16 -0400

    Add logics, now playable in the console.

--- script.js
+++ script.js
@@ -9,10 +9,103 @@ const Gameboard = (function () {
   // function that shows the current board.
   //   const getBoard = () => gameBoard;
 
-  return { gameBoard };
+  function setBoard(row, column, marker) {
+    gameBoard[row][column] = marker;
+  }
+
+  const getBoard = () => gameBoard;
+
+  return { getBoard, setBoard };
 })();
 
 // Player object
 const createPlayer = function (name, marker) {
   return { name, marker };
+};
+
+const createGame = function (player1, player2) {
+  let board = Gameboard.getBoard();
+  const play = function (player, row, column) {
+    if (board[row][column] === null) {
+      Gameboard.setBoard(row, column, player.marker);
+    } else {
+      console.log("Already taken! Place again");
+    }
+    checkWinner();
+  };
+
+  const announceWinner = function () {
+    console.log("Somebody won");
+  };
+
+  const checkWinner = function () {
+    if (checkRows() || checkCols() || checkDiag()) {
+      announceWinner();
+    }
+  };
+
+  const checkRows = function () {
+    // check first row
+    if (
+      board[0][0] !== null &&
+      board[0][0] === board[0][1] &&
+      board[0][0] === board[0][2]
+    ) {
+      return true;
+    } else if (
+      board[1][0] !== null &&
+      board[1][0] === board[1][1] &&
+      board[1][0] === board[1][2]
+    ) {
+      // check second row
+      return true;
+    } else if (
+      board[2][0] !== null &&
+      board[2][0] === board[2][1] &&
+      board[2][0] === board[2][2]
+    ) {
+      // check third row
+      return true;
+    }
+    return false;
+  };
+  const checkCols = function () {
+    // check first column
+    if (
+      board[0][0] !== null &&
+      board[0][0] === board[1][0] &&
+      board[0][0] === board[2][0]
+    ) {
+      return true;
+    } else if (
+      board[0][1] !== null &&
+      board[0][1] === board[1][1] &&
+      board[0][1] === board[2][1]
+    ) {
+      // check second column
+      return true;
+    } else if (
+      board[0][2] !== null &&
+      board[0][2] === board[1][2] &&
+      board[0][2] === board[2][2]
+    ) {
+      // check third column
+      return true;
+    }
+    return false;
+  };
+  const checkDiag = function () {
+    // check first diag
+    if (board[1][1] !== null) {
+      if (
+        (board[1][1] === board[0][0] && board[1][1] === board[2][2]) ||
+        (board[1][1] === board[2][0] && board[1][1] === board[0][2])
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  return { play };
 };
