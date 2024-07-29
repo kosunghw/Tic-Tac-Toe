51cc0a147dac1267ca7aec389437ed5389853466
Sung <sung272012@hotmail.com>
2024-07-29 15:08:19 -0400

    Fixed checkWinner function algorithm. Changed from hard coding if statements to for loops to organize the code.

--- script.js
+++ script.js
@@ -28,86 +28,66 @@ const createPlayer = function (name, marker) {
 
 const createGame = function (player1, player2) {
   let board = Gameboard.getBoard();
+  let moveCount = 0;
   const play = function (player, row, column) {
     if (board[row][column] === null) {
       Gameboard.setBoard(row, column, player.marker);
     } else {
       console.log("Already taken! Place again");
     }
-    checkWinner();
+    moveCount++;
+    checkWinner(moveCount, row, column, player);
   };
 
-  const checkWinner = function () {
-    // check first row
-    if (
-      board[0][0] !== null &&
-      board[0][0] === board[0][1] &&
-      board[0][0] === board[0][2]
-    ) {
-      board[0][0] === player1.marker
-        ? console.log(`${player1.name} won the game!`)
-        : console.log(`${player2.name} won the game!`);
-      Gameboard.resetBoard();
-    } else if (
-      board[1][0] !== null &&
-      board[1][0] === board[1][1] &&
-      board[1][0] === board[1][2]
-    ) {
-      // check second row
-      board[1][0] === player1.marker
-        ? console.log(`${player1.name} won the game!`)
-        : console.log(`${player2.name} won the game!`);
-      Gameboard.resetBoard();
-    } else if (
-      board[2][0] !== null &&
-      board[2][0] === board[2][1] &&
-      board[2][0] === board[2][2]
-    ) {
-      // check third row
-      board[2][0] === player1.marker
-        ? console.log(`${player1.name} won the game!`)
-        : console.log(`${player2.name} won the game!`);
-      Gameboard.resetBoard();
-    } else if (
-      // Check first column
-      board[0][0] !== null &&
-      board[0][0] === board[1][0] &&
-      board[0][0] === board[2][0]
-    ) {
-      board[0][0] === player1.marker
-        ? console.log(`${player1.name} won the game!`)
-        : console.log(`${player2.name} won the game!`);
-      Gameboard.resetBoard();
-    } else if (
-      board[0][1] !== null &&
-      board[0][1] === board[1][1] &&
-      board[0][1] === board[2][1]
-    ) {
-      // check second column
-      board[0][1] === player1.marker
-        ? console.log(`${player1.name} won the game!`)
-        : console.log(`${player2.name} won the game!`);
-      Gameboard.resetBoard();
-    } else if (
-      board[0][2] !== null &&
-      board[0][2] === board[1][2] &&
-      board[0][2] === board[2][2]
-    ) {
-      // check third column
-      board[0][2] === player1.marker
-        ? console.log(`${player1.name} won the game!`)
-        : console.log(`${player2.name} won the game!`);
-      Gameboard.resetBoard();
-    } else if (board[1][1] !== null) {
-      // Check diagonal
-      if (
-        (board[1][1] === board[0][0] && board[1][1] === board[2][2]) ||
-        (board[1][1] === board[2][0] && board[1][1] === board[0][2])
-      ) {
-        board[1][1] === player1.marker
-          ? console.log(`${player1.name} won the game!`)
-          : console.log(`${player2.name} won the game!`);
-        Gameboard.resetBoard();
+  const checkWinner = function (moveCount, row, column, player) {
+    // Announce draw if move count is greater than or equal to 9
+    if (moveCount >= 9) {
+      console.log(`DRAW`);
+    }
+
+    // Check end conditions
+
+    // Check column
+    for (let i = 0; i < 3; i++) {
+      if (board[row][i] !== player.marker) {
+        break;
+      }
+      if (i === 2) {
+        console.log(`${player.name} has won the game!`);
+      }
+    }
+
+    // Check row
+    for (let i = 0; i < 3; i++) {
+      if (board[i][column] !== player.marker) {
+        break;
+      }
+      if (i === 2) {
+        console.log(`${player.name} has won the game!`);
+      }
+    }
+
+    // Check diag
+    if (row === column) {
+      for (let i = 0; i < 3; i++) {
+        if (board[i][i] !== player.marker) {
+          break;
+        }
+        if (i === 2) {
+          console.log(`${player.name} has won the game!`);
+        }
+      }
+    }
+
+    // Check anti-diag
+    if (row + column === 2) {
+      for (let i = 0; i < 3; i++) {
+        if (board[i][2 - i] !== player.marker) {
+          break;
+        }
+        if (i === 2) {
+          console.log(`${player.name} has won the game!`);
+        }
       }
     }
   };
